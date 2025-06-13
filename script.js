ff233da41998009795d6ebaba9a95758431e0915
accio-uday <udaymanideep2002@gmail.com>
2025-06-13 18:52:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
-const makeChange = (c) => {
-  // your name here
-};
+function makeChange(amount) {
+	const change = {};
 
+	// Quarters
+	change.q = Math.floor(amount / 25);
+	amount %= 25;
+
+	// Dime
+	change.d = Math.floor(amount / 10);
+	amount %= 10;
+
+	// Nickel
+	change.n = Math.floor(amount / 5);
+	amount %= 5;
+
+	// Penny
+	change.p = Math.floor(amount / 1);
+	amount %= 1;
+
+	return change;
+}
 // Do not the change the code below
 const c = prompt("Enter c: ");
 alert(JSON.stringify(makeChange(c)));
